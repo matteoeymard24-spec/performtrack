615ac7a5aed5d04f3a31aee9618d68d526918545
matteoeymard24-spec <matteo.eymard24@gmail.com>
2026-02-14 19:17:14 +0100

    Fix: allow JSX in .js files

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,27 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      include: '**/*.{jsx,js}',  // Accepte JSX dans les fichiers .js
+    })
+  ],
   build: {
     outDir: 'dist',
     sourcemap: false,
   },
   server: {
     port: 3000,
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /.*\.js$/,  // Traite tous les .js comme du JSX
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',  // Charge les .js comme du JSX
+      },
+    },
   },
 })
